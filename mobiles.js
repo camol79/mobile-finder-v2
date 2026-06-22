b3b2c5ea11425d60a8ef24a90107a1d5e67b1c82
camol79 <amolchodankar555@gmail.com>
2026-06-22 21:35:33 +0530

    Add files via upload

---
+++ mobiles.js
@@ -0,0 +1,83 @@
+const mobiles = [
+
+{
+name:"Samsung Galaxy A36 5G",
+brand:"Samsung",
+price:22999,
+camera:"Excellent",
+display:"AMOLED",
+ram:"8GB",
+storage:"256GB"
+},
+
+{
+name:"Samsung Galaxy M16 5G",
+brand:"Samsung",
+price:14999,
+camera:"Good",
+display:"Full HD+",
+ram:"6GB",
+storage:"128GB"
+},
+
+{
+name:"OnePlus Nord CE 5",
+brand:"OnePlus",
+price:24999,
+camera:"Excellent",
+display:"120Hz AMOLED",
+ram:"8GB",
+storage:"256GB"
+},
+
+{
+name:"Nothing Phone 3a",
+brand:"Nothing",
+price:27999,
+camera:"Excellent",
+display:"AMOLED",
+ram:"8GB",
+storage:"256GB"
+},
+
+{
+name:"Moto Edge 60 Fusion",
+brand:"Motorola",
+price:22999,
+camera:"Excellent",
+display:"120Hz AMOLED",
+ram:"8GB",
+storage:"256GB"
+},
+
+{
+name:"Realme P3 Pro",
+brand:"Realme",
+price:21999,
+camera:"Good",
+display:"AMOLED",
+ram:"8GB",
+storage:"128GB"
+},
+
+{
+name:"Redmi Note 14 Pro",
+brand:"Xiaomi",
+price:24999,
+camera:"Excellent",
+display:"120Hz AMOLED",
+ram:"8GB",
+storage:"256GB"
+},
+
+{
+name:"iPhone 16",
+brand:"Apple",
+price:74999,
+camera:"Excellent",
+display:"Premium Flagship Display",
+ram:"8GB",
+storage:"128GB"
+}
+
+];
